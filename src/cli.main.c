5cba5d10f119f68915614fee38be7a78d9ae0b21
Stefanos Mitropoulos <stefmitropoulos@gmail.com>
2020-04-13 20:06:05 +0300

    added cli tool

---
+++ src/cli.main.c
@@ -0,0 +1,16 @@
+#define MAGIC_PACKET_LENGTH 102
+
+int main(int argc, char **argv) {
+
+    if ( argc != 2 )
+    {
+        perror("Please provide one and only mac address\n");
+        exit(-1);
+    }
+
+    unsigned char packet[MAGIC_PACKET_LENGTH];
+    createMagicPacket(packet, argv[1]);
+    sendWOLPacket(packet);
+    printf("Packet sent!\n");
+
+}
